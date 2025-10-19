2989068b5487b65a9fb8002dd7a36f3d493b0d57
Robert Yap <yap.rtim@gmail.com>
2025-10-20 01:56:51 +0800

    Add role picking logic and display

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { createApp } from 'https://unpkg.com/petite-vue@0.4.1/dist/petite-vue.es
 createApp({
     randomVar: "hello!!!!!",
     roles: [],
+    selectedRole: null,
 
     async mounted() {
         try {
@@ -11,5 +12,32 @@ createApp({
         } catch (error) {
             console.error(`Something went wrong while fetching roles: ${error}`)
         }
+
+        // Sort roles in alphabetical order
+        this.roles = this.roles.sort((a, b) => a.name.localeCompare(b.name))
+    },
+
+    // This way is super hacky, but it's the only way I know how to interact with
+    // the JS behavior of the FlowBite sidebar component. Will do some more research later.
+    // or will implement my own sidebar.
+    closeSidebar() {
+        const drawerCloseButton = document.getElementById("draw-navigation-close");
+        if (drawerCloseButton) {
+            if (drawerCloseButton.onclick) {
+                drawerCloseButton.onclick();
+            } else if (drawerCloseButton.click) {
+                drawerCloseButton.click();
+            }
+        }
+    },
+
+    selectRole(role) {
+        this.selectedRole = role;
+        this.closeSidebar();
+    },
+
+    convertAltName(name) {
+        console.log('in here')
+        return name.toLowerCase().replace(" ", "-");
     }
 }).mount()
